f44ddb215c0a42a877ff86f1104f22bf5cc74f6f
David Herrera <deivid.wgh@gmail.com>
2020-11-11 12:27:49 +0100

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -25,7 +25,6 @@ export class InfoPaginaService {
 
         this.cargada = true;
         this.info = resp;
-        // console.log(resp['email']);
       });
   }
 
@@ -35,7 +34,6 @@ export class InfoPaginaService {
       .subscribe( (resp: any[]) => {
 
         this.equipo = resp;
-        console.log(resp);
       });
   }
 
